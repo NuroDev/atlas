7cadba1fd753f38243a49726b484a0b1ea5a4626
N U R O ™ <bendixon50@gmail.com>
2019-07-11 20:59:51 +0100

    :recycle: Added event class

---
+++ atlas/src/event.rs
@@ -0,0 +1,59 @@
+use winit::{event::{Event, WindowEvent},
+            event_loop::{ControlFlow, EventLoop},
+            window::WindowBuilder};
+
+#[derive(Debug)]
+pub struct Instance {
+	pub title: String,
+	pub width: u32,
+	pub height: u32,
+}
+
+impl Default for Instance {
+	fn default() -> Instance {
+		Instance {
+			title: "Atlas".to_owned(),
+			width: 1024,
+			height: 768,
+		}
+	}
+}
+
+impl Instance {
+	pub fn new() -> Self { Self::default() }
+
+	pub fn resolution(mut self, width: u32, height: u32) -> Instance {
+		self.width = width;
+		self.height = height;
+		self
+	}
+
+	pub fn title(mut self, title: String) -> Instance {
+		self.title = title;
+		self
+	}
+
+	pub fn start(&self) {
+		let event_loop = EventLoop::new();
+		let window = WindowBuilder::new()
+			.with_title(&self.title)
+			.with_inner_size((self.width, self.height).into())
+			.build(&event_loop)
+			.expect("Failed to unwrap window builder");
+
+		event_loop.run(move |event, _, control_flow| match event {
+			Event::EventsCleared => {
+				window.request_redraw();
+			},
+			Event::WindowEvent {
+				event: WindowEvent::RedrawRequested,
+				..
+			} => {},
+			Event::WindowEvent {
+				event: WindowEvent::CloseRequested,
+				..
+			} => *control_flow = ControlFlow::Exit,
+			_ => *control_flow = ControlFlow::Wait,
+		});
+	}
+}
--- atlas/src/lib.rs
+++ atlas/src/lib.rs
@@ -9,41 +9,14 @@ extern crate gfx_backend_metal as backend;
 extern crate gfx_backend_vulkan as backend;
 extern crate gfx_hal as hal;
 
-use winit::{event::{Event, WindowEvent},
-            event_loop::{ControlFlow, EventLoop},
-            window::WindowBuilder};
+mod event;
+
+use event::Instance as EventInstance;
 
 pub fn init((width, height): (u32, u32), title: String) {
-	info!("Initializing Core...");
+	info!("Initializing Atlas...");
 
-	let event_loop = EventLoop::new();
-	let window = WindowBuilder::new()
-		.with_title(title)
-		.with_inner_size((width, height).into())
-		.build(&event_loop)
-		.expect("Failed to unwrap window builder");
+	let instance = EventInstance::new().title(title).resolution(width, height);
 
-	event_loop.run(move |event, _, control_flow| match event {
-		Event::EventsCleared => {
-				// Application update code.
-	
-				// Queue a RedrawRequested event.
-				window.request_redraw();
-		},
-		Event::WindowEvent {
-				event: WindowEvent::RedrawRequested,
-				..
-		} => {
-				// Redraw the application.
-				//
-				// It's preferrable to render in this event rather than in EventsCleared, since
-				// rendering in here allows the program to gracefully handle redraws requested
-				// by the OS.
-		},
-		Event::WindowEvent {
-				event: WindowEvent::CloseRequested,
-				..
-		} => *control_flow = ControlFlow::Exit,
-		_ => *control_flow = ControlFlow::Wait,
-	});
+	instance.start();
 }
