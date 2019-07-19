1a3b542f784f7b778be282091ac58c7fc3c205ba
N U R O ™ <bendixon50@gmail.com>
2019-07-19 21:11:21 +0100

    :fire: Removed old gfx crate imports

--- src/lib.rs
+++ src/lib.rs
@@ -91,12 +91,6 @@ extern crate quick_error;
 
 // Module exports
 extern crate gfx_backend_dx12 as backend;
-#[cfg(target_os = "macos")]
-extern crate gfx_backend_metal as backend;
-#[cfg(all(unix, not(target_os = "macos")))]
-extern crate gfx_backend_vulkan as backend;
-extern crate gfx_hal as hal;
-
 pub mod error;
 pub mod event;
 pub mod graphics;
