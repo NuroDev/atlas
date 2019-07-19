9c8e4481842e25e8dfdbf126ea8a0463db5c80a4
N U R O ™ <bendixon50@gmail.com>
2019-07-19 21:12:57 +0100

    :tada: Added initial error handler types

--- src/error.rs
+++ src/error.rs
@@ -1,9 +1,48 @@
 quick_error! {
 	#[derive(Debug)]
 	pub enum AtlasError {
+		/// TODO: Documentation
+		Audio(message: String) {
+		  description("Audio Error")
+		  display("Audio Error: {}", message)
+		}
+		/// TODO: Documentation
+		Config(message: String) {
+		  description("Config Error")
+		  display("Config Error: {}", message)
+		}
+		/// TODO: Documentation
 		Event(message: String) {
-		  description("Text")
-		  display("Text: {}", message)
+		  description("Event Error")
+		  display("Event Error: {}", message)
+		}
+		/// TODO: Documentation
+		Font(message: String) {
+		  description("Font Error")
+		  display("Font Error: {}", message)
+		}
+		/// TODO: Documentation
+		Graphics(message: String) {
+		  description("Graphics Error")
+		  display("Graphics Error: {}", message)
+		}
+		/// TODO: Documentation
+		Input(message: String) {
+		  description("Input Error")
+		  display("Input Error: {}", message)
+		}
+		/// TODO: Documentation
+		Resource(message: String) {
+		  description("Resource Error")
+		  display("Resource Error: {}", message)
+		}
+		/// TODO: Documentation
+		Window(message: String) {
+		  description("Window Error")
+		  display("Window Error: {}", message)
 		}
 	}
 }
+
+/// TODO: Documentation
+pub type Result<T=()> = std::result::Result<T, AtlasError>;
