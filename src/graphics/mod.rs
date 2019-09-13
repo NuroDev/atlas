1d080e65e8ff87de338a3b3ab7c382a7882d938d
N U R O ™ <me@nuro.sh>
2019-09-13 18:58:34 +0100

    :wrench: Graphics backends use feature flags rather than config flags

--- src/graphics/mod.rs
+++ src/graphics/mod.rs
@@ -1,7 +1,13 @@
-#[cfg(windows)]
+#[cfg(feature = "dx11")]
+extern crate gfx_backend_dx11 as backend;
+
+#[cfg(feature = "dx12")]
 extern crate gfx_backend_dx12 as backend;
-#[cfg(target_os = "macos")]
+
+#[cfg(feature = "metal")]
 extern crate gfx_backend_metal as backend;
-#[cfg(all(unix, not(target_os = "macos")))]
+
+#[cfg(feature = "vulkan")]
 extern crate gfx_backend_vulkan as backend;
+
 extern crate gfx_hal as hal;
