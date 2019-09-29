772b41ab1b7f05d373a58237056499e943fc251d
N U R O ™ <me@nuro.sh>
2019-09-29 18:45:44 +0100

    :sparkles: Added empty graphics backend for failover

--- src/graphics/mod.rs
+++ src/graphics/mod.rs
@@ -1,5 +1,5 @@
 // TODO: Removed extern crate (Rust 2018)
-extern crate gfx_hal as hal;
+// extern crate gfx_hal as hal;
 
 #[cfg(feature = "dx11")]
 pub use gfx_backend_dx11::Instance;
@@ -12,3 +12,11 @@ pub use gfx_backend_metal::Instance;
 
 #[cfg(feature = "vulkan")]
 pub use gfx_backend_vulkan::Instance;
+
+#[cfg(not(any(
+    feature = "dx11",
+    feature = "dx12",
+    feature = "metal",
+    feature = "vulkan"
+)))]
+pub use gfx_backend_empty::Instance;
